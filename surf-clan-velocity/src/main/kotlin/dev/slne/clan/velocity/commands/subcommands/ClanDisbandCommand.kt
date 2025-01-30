bad7cbe5470b9f9a6f80584faf5fef1409d55e51
Ammo <ammo@slne.dev>
2025-01-30 21:51:55 +0100

    make clan disband confirmable and block if clan.size > 50

--- surf-clan-velocity/src/main/kotlin/dev/slne/clan/velocity/commands/subcommands/ClanDisbandCommand.kt
+++ surf-clan-velocity/src/main/kotlin/dev/slne/clan/velocity/commands/subcommands/ClanDisbandCommand.kt
@@ -2,8 +2,8 @@ package dev.slne.clan.velocity.commands.subcommands
 
 import com.github.shynixn.mccoroutine.velocity.launch
 import dev.jorel.commandapi.CommandAPICommand
-import dev.jorel.commandapi.arguments.StringArgument
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
+import dev.jorel.commandapi.kotlindsl.stringArgument
 import dev.slne.clan.api.permission.ClanPermission
 import dev.slne.clan.core.*
 import dev.slne.clan.core.service.ClanService
@@ -24,69 +24,7 @@ class ClanDisbandCommand(clanService: ClanService) : CommandAPICommand("disband"
     init {
         withPermission("surf.clan.disband")
 
-        withOptionalArguments(object : StringArgument("confirm") {
-            init {
-                withPermission("surf.clan.disband.confirm")
-
-                executesPlayer(PlayerCommandExecutor { player, _ ->
-                    val clan = player.findClan(clanService)
-
-                    if (clan == null) {
-                        player.sendMessage(Messages.notInClanComponent)
-
-                        return@PlayerCommandExecutor
-                    }
-
-                    if (!clan.hasPermission(player, ClanPermission.DISBAND)) {
-                        player.sendMessage(buildMessage {
-                            append(
-                                Component.text("Du hast keine Berechtigung, den Clan ", COLOR_ERROR)
-                            )
-                            append(clanComponent(clan))
-                            append(Component.text(" aufzulösen.", COLOR_ERROR))
-                        })
-
-                        return@PlayerCommandExecutor
-                    }
-
-                    if (clan.members.size > CLAN_MAX_MEMBERS_DISBAND) {
-                        player.sendMessage(buildMessage {
-                            append(Component.text("Du kannst den Clan ", COLOR_ERROR))
-                            append(clanComponent(clan))
-                            append(Component.text(" nicht auflösen, da er mehr als ", COLOR_ERROR))
-                            append(
-                                Component.text(
-                                    CLAN_MAX_MEMBERS_DISBAND.toString(),
-                                    COLOR_VARIABLE
-                                )
-                            )
-                            append(
-                                Component.text(
-                                    " Mitglieder hat. Wende dich bitte an den Support.",
-                                    COLOR_ERROR
-                                )
-                            )
-                        })
-
-                        return@PlayerCommandExecutor
-                    }
-
-                    val clanDisbandedMessage = buildMessage {
-                        append(Component.text("Der Clan ", COLOR_ERROR))
-                        append(clanComponent(clan))
-                        append(Component.text(" wurde aufgelöst.", COLOR_ERROR))
-                    }
-
-                    plugin.container.launch {
-                        clanService.deleteClan(clan)
-
-                        clan.members.forEach { member ->
-                            member.player.sendMessage(clanDisbandedMessage)
-                        }
-                    }
-                })
-            }
-        })
+        stringArgument("confirm", true)
 
         executesPlayer(PlayerCommandExecutor { player, args ->
             val clan = player.findClan(clanService)
@@ -124,6 +62,23 @@ class ClanDisbandCommand(clanService: ClanService) : CommandAPICommand("disband"
                 })
 
                 return@PlayerCommandExecutor
+            }
+
+            val confirm = args.getOrDefaultUnchecked("confirm", "")
+            if (confirm.isNotEmpty() && confirm == "confirm") {
+                val clanDisbandedMessage = buildMessage {
+                    append(Component.text("Der Clan ", COLOR_ERROR))
+                    append(clanComponent(clan))
+                    append(Component.text(" wurde aufgelöst.", COLOR_ERROR))
+                }
+
+                plugin.container.launch {
+                    clanService.deleteClan(clan)
+
+                    clan.members.forEach { member ->
+                        member.player.sendMessage(clanDisbandedMessage)
+                    }
+                }
             }
 
             player.sendMessage(buildMessage {
