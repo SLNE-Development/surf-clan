dda15e8fcae88aca99dd13a7a0c8123ada03579b
Ammo <ammo@slne.dev>
2025-01-30 21:25:01 +0100

    added clan leave command

--- surf-clan-velocity/src/main/kotlin/dev/slne/clan/velocity/commands/ClanCommand.kt
+++ surf-clan-velocity/src/main/kotlin/dev/slne/clan/velocity/commands/ClanCommand.kt
@@ -4,6 +4,7 @@ import dev.jorel.commandapi.CommandAPICommand
 import dev.slne.clan.core.service.ClanService
 import dev.slne.clan.velocity.commands.subcommands.ClanCreateCommand
 import dev.slne.clan.velocity.commands.subcommands.ClanDisbandCommand
+import dev.slne.clan.velocity.commands.subcommands.ClanLeaveCommand
 import dev.slne.clan.velocity.commands.subcommands.member.ClanKickMemberCommand
 import dev.slne.clan.velocity.commands.subcommands.member.invite.ClanInviteCommand
 import dev.slne.clan.velocity.commands.subcommands.member.role.ClanDemoteMemberCommand
@@ -17,5 +18,6 @@ class ClanCommand(clanService: ClanService) : CommandAPICommand("clan") {
         withSubcommand(ClanDemoteMemberCommand(clanService))
         withSubcommand(ClanDisbandCommand(clanService))
         withSubcommand(ClanKickMemberCommand(clanService))
+        withSubcommand(ClanLeaveCommand(clanService))
     }
 }
---
+++ surf-clan-velocity/src/main/kotlin/dev/slne/clan/velocity/commands/subcommands/ClanLeaveCommand.kt
@@ -0,0 +1,158 @@
+package dev.slne.clan.velocity.commands.subcommands
+
+import com.github.shynixn.mccoroutine.velocity.launch
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.executors.PlayerCommandExecutor
+import dev.slne.clan.core.*
+import dev.slne.clan.core.service.ClanService
+import dev.slne.clan.core.utils.clanComponent
+import dev.slne.clan.velocity.extensions.findClan
+import dev.slne.clan.velocity.extensions.player
+import dev.slne.clan.velocity.extensions.playerOrNull
+import dev.slne.clan.velocity.extensions.realName
+import dev.slne.clan.velocity.plugin
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.event.HoverEvent
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+
+class ClanLeaveCommand(clanService: ClanService) : CommandAPICommand("leave") {
+    init {
+        withPermission("surf.clan.leave")
+
+        withSubcommand(object : CommandAPICommand("confirm") {
+            init {
+                withPermission("surf.clan.leave.confirm")
+
+                executesPlayer(PlayerCommandExecutor { player, _ ->
+                    val clan = player.findClan(clanService)
+
+                    if (clan == null) {
+                        player.sendMessage(Messages.notInClanComponent)
+
+                        return@PlayerCommandExecutor
+                    }
+
+                    val clanDisbandedMessage = buildMessage {
+                        append(Component.text("Der Clan ", COLOR_INFO))
+                        append(clanComponent(clan))
+                        append(Component.text(" wurde aufgelöst, da der Anführer ", COLOR_INFO))
+                        append(player.realName())
+                        append(Component.text(" den Clan verlassen hat.", COLOR_INFO))
+                    }
+
+                    if (clan.createdBy == player.uniqueId) {
+                        plugin.container.launch {
+                            clanService.deleteClan(clan)
+
+                            clan.members.forEach { member ->
+                                member.player.sendMessage(clanDisbandedMessage)
+                            }
+                        }
+                    } else {
+                        val clanMember = clan.members.find { it.player.uniqueId == player.uniqueId }
+
+                        if (clanMember == null) {
+                            player.sendMessage(Messages.notInClanComponent)
+
+                            return@PlayerCommandExecutor
+                        }
+
+                        plugin.container.launch {
+                            clan.removeMember(clanMember)
+                            clanService.saveClan(clan)
+
+                            clan.members.forEach { member ->
+                                member.playerOrNull?.sendMessage(buildMessage {
+                                    append(Component.text("Der Spieler ", COLOR_INFO))
+                                    append(player.realName())
+                                    append(Component.text(" hat den Clan verlassen.", COLOR_INFO))
+                                })
+                            }
+
+                            player.sendMessage(buildMessage {
+                                append(Component.text("Du hast den Clan ", COLOR_SUCCESS))
+                                append(clanComponent(clan))
+                                append(Component.text(" verlassen.", COLOR_SUCCESS))
+                            })
+                        }
+                    }
+                })
+            }
+        })
+
+        executesPlayer(PlayerCommandExecutor { player, args ->
+            val clan = player.findClan(clanService)
+
+            if (clan == null) {
+                player.sendMessage(Messages.notInClanComponent)
+
+                return@PlayerCommandExecutor
+            }
+
+            player.sendMessage(buildMessage {
+                append(Component.text("Möchtest du den Clan ", COLOR_INFO))
+                append(clanComponent(clan))
+                append(Component.text(" wirklich verlassen? Klicke ", COLOR_INFO))
+                append(buildMessage(false) {
+                    append(Component.text("hier", COLOR_VARIABLE, TextDecoration.BOLD))
+                    hoverEvent(HoverEvent.showText(buildMessage(false) {
+                        append(
+                            Component.text(
+                                "Klicke hier, um zu bestätigen.",
+                                NamedTextColor.GRAY
+                            )
+                        )
+                        appendNewline()
+                        appendNewline()
+                        appendNewline()
+
+                        append(Component.text("Achtung: ", NamedTextColor.RED, TextDecoration.BOLD))
+                        append(
+                            Component.text(
+                                "Du kannst den Vorgang nicht rückgängig machen.",
+                                NamedTextColor.RED
+                            )
+                        )
+                        appendNewline()
+                        appendNewline()
+
+                        append(
+                            Component.text(
+                                "Wenn du den Clan verlässt, verlierst du alle Rechte ",
+                                NamedTextColor.RED
+                            )
+                        )
+                        appendNewline()
+                        append(
+                            Component.text(
+                                "und kannst nicht mehr selbstständig in den Clan zurückkehren.",
+                                NamedTextColor.RED
+                            )
+                        )
+                        appendNewline()
+                        appendNewline()
+
+                        append(
+                            Component.text(
+                                "Wenn du der Besitzer des Clans bist ",
+                                NamedTextColor.RED,
+                                TextDecoration.BOLD
+                            )
+                        )
+                        append(
+                            Component.text(
+                                "und ihn verlässt, wird der Clan aufgelöst.",
+                                NamedTextColor.RED,
+                                TextDecoration.BOLD
+                            )
+                        )
+                    }))
+                    clickEvent(ClickEvent.suggestCommand("/clan leave confirm"))
+                })
+                append(Component.text(" um zu bestätigen.", COLOR_INFO))
+            })
+        })
+    }
+}
