810c07721dcd0ac81382abc45e861b77bcfdd738
Ammo <ammo@slne.dev>
2025-03-18 12:37:25 +0100

    chore: Remove buildSrc as its not used anymore due to surfapi

--- buildSrc/src/main/kotlin/common-conventions.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-//    "implementation"(platform("org.springframework.boot:spring-boot-dependencies:3.3.6"))
-//
-//    "compileOnly"("org.springframework.boot:spring-boot-configuration-processor:3.3.6")
-//    "kapt"("org.springframework.boot:spring-boot-configuration-processor:3.3.6")
-}
-
-group = "dev.slne.surf.clan"
-version = findProperty("version") as String? ?: "1.0.0-SNAPSHOT"
