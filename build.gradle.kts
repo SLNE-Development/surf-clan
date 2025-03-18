1c84f64453c4d6fac7ce47f820193305f6edc3cc
Ammo <ammo@slne.dev>
2025-03-18 12:37:54 +0100

    fix: Update surf api gradle plugin version to use latest stable release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-unsafe/") { name = "maven-unsafe" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.4-1.0.90-SNAPSHOT")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.4+")
     }
 }
