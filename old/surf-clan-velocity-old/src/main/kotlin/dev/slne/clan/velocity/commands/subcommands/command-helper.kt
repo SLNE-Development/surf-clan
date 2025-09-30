dd40a62f702b8301b0f492184637a242644f7cce
Ammo <ammo@slne.dev>
2025-09-30 12:50:50 +0200

    chore: Remove old velocity commands

--- old/surf-clan-velocity-old/src/main/kotlin/dev/slne/clan/velocity/commands/subcommands/command-helper.kt
+++
@@ -1,9 +0,0 @@
-package dev.slne.clan.velocity.commands.subcommands
-
-import dev.jorel.commandapi.arguments.Argument
-import dev.jorel.commandapi.arguments.ArgumentSuggestions
-import dev.slne.clan.core.service.ClanService
-
-fun Argument<*>.includeClanTagSuggestions(clanService: ClanService) {
-    includeSuggestions(ArgumentSuggestions.stringCollection { _ -> clanService.clans.map { it.tag } })
-}
