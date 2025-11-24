cce76a1cb3b44e0e105c7024bdf87293b1cc6a0f
Jo_field <150937665+JoField08@users.noreply.github.com>
2025-11-24 21:17:02 +0100

    updated command for dialog

--- surf-clan-paper/src/main/kotlin/dev/slne/surf/clan/paper/commands/ClanCommand.kt
+++ surf-clan-paper/src/main/kotlin/dev/slne/surf/clan/paper/commands/ClanCommand.kt
@@ -1,26 +1,25 @@
 package dev.slne.surf.clan.paper.commands
 
 import com.github.shynixn.mccoroutine.folia.launch
+import dev.jorel.commandapi.arguments.PlayerProfileArgument
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
-import dev.jorel.commandapi.kotlindsl.getValue
-import dev.jorel.commandapi.kotlindsl.playerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import dev.slne.surf.clan.paper.dialogs.MainClanDialog
 import dev.slne.surf.clan.paper.permissions.Permissions
 import dev.slne.surf.clan.paper.plugin
-import org.bukkit.entity.Player
+import dev.slne.surf.surfapi.bukkit.api.extensions.server
 
 fun clanCommand() = commandAPICommand("clan") {
     withPermission(Permissions.COMMAND_GENERIC)
 
-    playerArgument("targetPlayer", optional = true)
+    withArguments(PlayerProfileArgument("targetPlayer").setOptional(true))
 
-    playerExecutor { executor, args ->
-        val targetPlayer: Player? by args
-        val usableTargetPlayer = targetPlayer ?: executor
+    playerExecutor { executor, arguments ->
+        val targetProfile = arguments.getOrDefaultUnchecked("targetPlayer", executor.playerProfile)
+        val targetOffline = server.getOfflinePlayer(targetProfile.id!!)
 
         plugin.launch {
-            executor.showDialog(MainClanDialog.createDialog(executor, usableTargetPlayer))
+            executor.showDialog(MainClanDialog.createDialog(executor, targetOffline))
         }
     }
 }
