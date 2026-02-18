15d14715e92cf614ded510d27f18bc72655f5497
twisti <denizgebauer@web.de>
2026-02-18 17:35:49 +0100

    [ci skip] feat: Add SLNE releases repository to publishing configuration in build script

--- surf-clan-api/build.gradle.kts
+++ surf-clan-api/build.gradle.kts
@@ -1,7 +1,15 @@
+import dev.slne.surf.surfapi.gradle.util.slneReleases
+
 plugins {
     id("dev.slne.surf.surfapi.gradle.velocity")
 }
 
 dependencies {
     api("dev.slne.surf.bitmap:surf-bitmap-provider-common:2.1.3-SNAPSHOT")
+}
+
+publishing {
+    repositories {
+        slneReleases()
+    }
 }
