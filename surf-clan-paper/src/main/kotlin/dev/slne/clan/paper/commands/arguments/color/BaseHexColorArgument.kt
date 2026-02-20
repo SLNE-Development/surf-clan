b6bc29a100d566b288910a0014f303573eb88834
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-21 00:06:07 +0100

    fix: Update color suggestion format to remove hash prefix for hex values

--- surf-clan-paper/src/main/kotlin/dev/slne/clan/paper/commands/arguments/color/BaseHexColorArgument.kt
+++ surf-clan-paper/src/main/kotlin/dev/slne/clan/paper/commands/arguments/color/BaseHexColorArgument.kt
@@ -33,25 +33,25 @@ abstract class BaseHexColorArgument<T>(
         replaceSuggestions(ArgumentSuggestions.stringCollection { _ ->
             if (requiredLength == 6) {
                 listOf(
-                    "#FF6B6B",
-                    "#4ECDC4",
-                    "#1A1A2E",
-                    "#F7B801",
-                    "#6A4C93",
-                    "#00C2FF",
-                    "#2EC4B6",
-                    "#E71D36"
+                    "FF6B6B",
+                    "4ECDC4",
+                    "1A1A2E",
+                    "F7B801",
+                    "6A4C93",
+                    "00C2FF",
+                    "2EC4B6",
+                    "E71D36"
                 )
             } else {
                 listOf(
-                    "#FF6B6BCC",
-                    "#4ECDC480",
-                    "#1A1A2ECC",
-                    "#F7B80199",
-                    "#6A4C93B3",
-                    "#00C2FF66",
-                    "#2EC4B6AA",
-                    "#E71D3688"
+                    "FF6B6BCC",
+                    "4ECDC480",
+                    "1A1A2ECC",
+                    "F7B80199",
+                    "6A4C93B3",
+                    "00C2FF66",
+                    "2EC4B6AA",
+                    "E71D3688"
                 )
             }
         })
