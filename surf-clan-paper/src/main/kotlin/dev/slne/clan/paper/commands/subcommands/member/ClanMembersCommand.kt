5d3601914969d55e95238e5dd57722fa0c61943a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-28 17:16:29 +0200

    ✨ feat: Enhance clan member data with current server information and improve pagination display

--- surf-clan-paper/src/main/kotlin/dev/slne/clan/paper/commands/subcommands/member/ClanMembersCommand.kt
+++ surf-clan-paper/src/main/kotlin/dev/slne/clan/paper/commands/subcommands/member/ClanMembersCommand.kt
@@ -10,18 +10,22 @@ import dev.slne.clan.paper.commands.arguments.ClanByClanTagArgument
 import dev.slne.clan.paper.permission.ClanPermissions
 import dev.slne.surf.api.core.command.args.awaitingOrNull
 import dev.slne.surf.api.core.font.toSmallCaps
-import dev.slne.surf.api.core.messages.Colors
 import dev.slne.surf.api.core.messages.adventure.buildText
 import dev.slne.surf.api.core.messages.pagination.Pagination
 import dev.slne.surf.api.core.service.PlayerLookupService
 import dev.slne.surf.api.paper.command.executors.playerExecutorSuspend
 import dev.slne.surf.clan.core.clan.ClanImpl
 import dev.slne.surf.clan.core.client.components.Components
+import dev.slne.surf.core.api.common.SurfCoreApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 import java.util.concurrent.ConcurrentHashMap
 
-data class ClanMemberData(val memberName: String, val role: ClanMemberRole)
+data class ClanMemberData(
+    val memberName: String,
+    val role: ClanMemberRole,
+    val currentServer: String?
+)
 
 private suspend fun pagination(clan: ClanImpl): Pagination<ClanMemberData> {
     val clanInformationHover = Components.Clan.renderClanInformationHover(clan)
@@ -37,7 +41,19 @@ private suspend fun pagination(clan: ClanImpl): Pagination<ClanMemberData> {
         rowRenderer { member, i ->
             listOf(
                 buildText {
-                    text(member.memberName, Colors.WHITE)
+                    if (member.currentServer != null) {
+                        append {
+                            success(member.memberName)
+                            hoverEvent(buildText {
+                                success("online auf ${member.currentServer}")
+                            })
+                        }
+                    } else {
+                        spacer(member.memberName)
+                        hoverEvent(buildText {
+                            spacer("offline")
+                        })
+                    }
                     appendSpace()
                     spacer("(")
                     append(member.role)
@@ -65,12 +81,19 @@ fun CommandAPICommand.clanMembersCommand() = subcommand("members") {
                 launch {
                     val name =
                         PlayerLookupService.getUsername(member.uuid) ?: member.uuid.toString()
-                    data.add(ClanMemberData(name, member.role))
+
+                    data.add(
+                        ClanMemberData(
+                            name,
+                            member.role,
+                            SurfCoreApi.getPlayer(member.uuid)?.currentServer?.displayName
+                        )
+                    )
                 }
             }
         }
 
         val pagination = pagination(clan as ClanImpl)
-        player.sendMessage(pagination.renderComponent(data))
+        player.sendMessage(pagination.renderComponent(data.sortedBy { it.currentServer }))
     }
 }
